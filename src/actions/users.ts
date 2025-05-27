4727eb90d6f5f19107c5eb93c34a4a20d66000b5
tyke01 <techvictor653@gmail.com>
2025-05-27 10:34:53 +0300

    update: add database user creation

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,53 +1,58 @@
-"use server"
+"use server";
 
 import { createClient } from "@/auth/server";
+import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
 
 export const loginAction = async (email: string, password: string) => {
-    try {
-        const {auth} = await createClient();
-        const {error}  = await auth.signInWithPassword({
-            email, password
-        });
+  try {
+    const { auth } = await createClient();
+    const { error } = await auth.signInWithPassword({
+      email,
+      password,
+    });
 
-        if (error) throw error;
-        return { errorMessage: null };
-
-    } catch (error) {
-        return handleError(error);
-    }
-}
-
+    if (error) throw error;
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
 
 export const signUpAction = async (email: string, password: string) => {
-    try {
-        const {auth} = await createClient();
-        const {data, error}  = await auth.signUp({
-            email, password
-        });
+  try {
+    const { auth } = await createClient();
+    const { data, error } = await auth.signUp({
+      email,
+      password,
+    });
 
-        if (error) throw error;
-        const userId = data.user?.id;
-        if (!userId) throw new Error("User ID not found after sign up");
+    if (error) throw error;
+    const userId = data.user?.id;
+    if (!userId) throw new Error("User ID not found after sign up");
 
-        // add user to the database
+    // add user to the database
+    await prisma.user.create({
+      data: {
+        id: userId,
+        email,
+      },
+    });
 
-        return { errorMessage: null };
-
-    } catch (error) {
-        return handleError(error);
-    }
-}
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
 
 export const logoutAction = async () => {
-    try {
-        const {auth} = await createClient();
-        const {error} = await auth.signOut();
+  try {
+    const { auth } = await createClient();
+    const { error } = await auth.signOut();
 
-        if (error) throw error;
-        return { errorMessage: null };
-
-    } catch (error) {
-        return handleError(error);
-    }
-}
+    if (error) throw error;
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
